7e2ee71fa873c1fa849269a6721b7dc8ef4818f4
Andy Potter <andy@andypotter.co.uk>
2023-03-08 18:06:57 +0000

    updates

--- shared_desktop/home/.config/nvim/lua/options/init.lua
+++ shared_desktop/home/.config/nvim/lua/options/init.lua
@@ -7,7 +7,6 @@ vim.opt.softtabstop = 4
 vim.opt.autoindent = true
 vim.opt.smartindent = true
 
-
 vim.opt.termguicolors = true
 
 vim.opt.mouse = "a"
@@ -22,5 +21,10 @@ vim.opt.background = "dark"
 vim.g.gruvbox_material_background = "hard"
 vim.cmd("colorscheme gruvbox-material")
 
--- Javascript 
-vim.api.nvim_command("autocmd FileType javascript,javascriptreact,javascript.jsx,typescript,typescriptreact,typescript.tsx,css,lua,json setlocal shiftwidth=2 tabstop=2 softtabstop=2");
+-- 2 Spaces
+vim.api.nvim_command(
+	"autocmd FileType c,cpp,javascript,javascriptreact,javascript.jsx,typescript,typescriptreact,typescript.tsx,css,lua,json setlocal shiftwidth=2 tabstop=2 softtabstop=2"
+)
+
+-- Actual Tabs
+vim.api.nvim_command("autocmd FileType make setlocal noexpandtab shiftwidth=8 tabstop=8 softtabstop=8")
